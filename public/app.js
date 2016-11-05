fb22906bd0f85814ed5dabead5ad2bb31c980685
Jecho Ricafrente <jecho@foxley.co>
2016-11-04 20:52:18 -0700

    added https to frontend interface

--- public/app.js
+++ public/app.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var config = require('./scripts/configController');
 
 var app = express();
+var options = {
+   key  : fs.readFileSync('./certs/server.key'),
+   cert : fs.readFileSync('./certs/server.crt')
+};
 
 app.use('/', express.static('.'));
 
@@ -13,6 +19,6 @@ app.get('/home', function (req, res) {
 	res.send('Hello World!');
 });
 
-app.listen(config.CLIENT_PORT, function () {
+https.createServer(options, app).listen(config.CLIENT_PORT, function () {
 	console.log('Example app listening on port ' + config.CLIENT_PORT + '!');
 });
