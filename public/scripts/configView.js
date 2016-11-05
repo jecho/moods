8d18336a37ccd0caace191375e1ce88032ad1636
Jecho Ricafrente <jecho@foxley.co>
2016-11-04 21:26:14 -0700

    modified frontend dns values, and added front end svc/deployment to yaml file

--- public/scripts/configView.js
+++ public/scripts/configView.js
@@ -1,8 +1,8 @@
 
 var config = {
 	NAME: 'app_engine',
-	REDIRECT: 'http://192.168.1.141:2000',
-	MOOD_URI: 'http://192.168.1.141:3000',
+	REDIRECT: 'http://moods-frontend',
+	MOOD_URI: 'http://moods-api',
 	MOOD_FREQUENCY: '/api/largest/',
 	MOOD_ALL: '/api/all',
 	MOOD_SUBMIT: '/api/submit'
